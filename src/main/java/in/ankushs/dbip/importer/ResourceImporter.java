2e629a6f38e1c14502adf2077d6c4017f0f66ab8
Nilay <nil.joshi860@gmail.com>
2021-12-17 09:14:24 +0530

    Changing csv column numbers as per new file format

--- src/main/java/in/ankushs/dbip/importer/ResourceImporter.java
+++ src/main/java/in/ankushs/dbip/importer/ResourceImporter.java
@@ -91,10 +91,10 @@ public final class ResourceImporter {
 						.Builder()
 						.withStartInetAddress(InetAddresses.forString(array[0]))
 						.withEndInetAddress(InetAddresses.forString(array[1]))
-						.withCountryCode(array[2])
-						.withCountry(CountryResolver.resolveToFullName(array[2]))
-						.withProvince(interner.intern(array[3]))
-						.withCity(interner.intern(array[4]))
+						.withCountryCode(array[3])
+						.withCountry(CountryResolver.resolveToFullName(array[3]))
+						.withProvince(interner.intern(array[4]))
+						.withCity(interner.intern(array[5]))
 						.withIsp(isp)
 						.build();
 
